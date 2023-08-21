c2f0e70955162139142f6bb428e42c5cfde25613
Lorias-Aegorin <Jannik.k11@gmail.com>
2023-08-21 18:09:44 +0200

    - added joda time dependency for more precise time calculation

--- src/main/java/com/jannik_kuehn/loritime/common/utils/TimeUtil.java
+++ src/main/java/com/jannik_kuehn/loritime/common/utils/TimeUtil.java
@@ -1,6 +1,10 @@
 package com.jannik_kuehn.loritime.common.utils;
 
 import com.jannik_kuehn.loritime.common.config.localization.Localization;
+import org.joda.time.Duration;
+import org.joda.time.Period;
+import org.joda.time.format.PeriodFormatter;
+import org.joda.time.format.PeriodFormatterBuilder;
 
 import java.util.Objects;
 
@@ -8,43 +12,36 @@ public final class TimeUtil {
 
     public static String formatTime(long seconds, Localization localization) {
         Objects.requireNonNull(localization);
-        long sec = seconds;
 
-        long min = sec / 60;
-        sec %= 60;
+        Duration duration = Duration.standardSeconds(seconds);
+        Period period = duration.toPeriodFrom(new org.joda.time.Instant(0)).normalizedStandard();
 
-        long h = min / 60;
-        min %= 60;
+        PeriodFormatter formatter = new PeriodFormatterBuilder()
+                .appendYears()
+                .appendSuffix(getLocalizedUnitString(period.getYears(), localization, "unit.year"))
+                .appendMonths()
+                .appendSuffix(getLocalizedUnitString(period.getMonths(), localization, "unit.month"))
+                .appendWeeks()
+                .appendSuffix(getLocalizedUnitString(period.getWeeks(), localization, "unit.week"))
+                .appendDays()
+                .appendSuffix(getLocalizedUnitString(period.getDays(), localization, "unit.day"))
+                .appendHours()
+                .appendSuffix(getLocalizedUnitString(duration.toStandardHours().getHours(), localization, "unit.hour"))
+                .appendMinutes()
+                .appendSuffix(getLocalizedUnitString(duration.toStandardMinutes().getMinutes(), localization, "unit.minute"))
+                .appendSeconds()
+                .appendSuffix(getLocalizedUnitString(duration.toStandardSeconds().getSeconds(), localization, "unit.second"))
+                .toFormatter();
 
-        long d = h / 24;
-        h %= 24;
-
-        long m = d / 30;
-        d %= 30;
-
-        long y = m / 12;
-        m %= 12;
-
-        long w = d / 7;
-        d %= 7;
-
-        String secStr =            sec + " " + (sec == 1 ? localization.getRawMessage("unit.second.singular") : localization.getRawMessage("unit.second.plural"))       ;
-        String minStr = min != 0 ? min + " " + (min == 1 ? localization.getRawMessage("unit.minute.singular") : localization.getRawMessage("unit.minute.plural")) : null;
-        String hStr   = h   != 0 ? h   + " " + (h   == 1 ? localization.getRawMessage("unit.hour.singular")   : localization.getRawMessage("unit.hour.plural"))   : null;
-        String dStr   = d   != 0 ? d   + " " + (d   == 1 ? localization.getRawMessage("unit.day.singular")    : localization.getRawMessage("unit.day.plural"))    : null;
-        String wStr   = w   != 0 ? w   + " " + (w   == 1 ? localization.getRawMessage("unit.week.singular")   : localization.getRawMessage("unit.week.plural"))   : null;
-        String mStr   = m   != 0 ? m   + " " + (m   == 1 ? localization.getRawMessage("unit.month.singular")  : localization.getRawMessage("unit.month.plural"))  : null;
-        String yStr   = y   != 0 ? y   + " " + (y   == 1 ? localization.getRawMessage("unit.year.singular")   : localization.getRawMessage("unit.year.plural"))   : null;
-
-        String r = (yStr   == null ? "" : yStr   + " ")
-                 + (mStr   == null ? "" : mStr   + " ")
-                 + (wStr   == null ? "" : wStr   + " ")
-                 + (dStr   == null ? "" : dStr   + " ")
-                 + (hStr   == null ? "" : hStr   + " ")
-                 + (minStr == null ? "" : minStr + " ")
-                 + (sec == 0 && seconds != 0 ? "" : secStr + " ");
-        return r.substring(0, r.length() - 1);
+        return formatter.print(period).replaceAll("\\s+", " ").trim();
     }
 
-    private TimeUtil() {}
+    private static String getLocalizedUnitString(long value, Localization localization, String unitKey) {
+        String unitMessageKey = unitKey + (value == 1 ? ".singular" : ".plural");
+        String unitString = localization.getRawMessage(unitMessageKey);
+        return (value == 0 ? "" : " ") + unitString + (value == 0 ? "" : " ");
+    }
+
+    private TimeUtil() {
+    }
 }
